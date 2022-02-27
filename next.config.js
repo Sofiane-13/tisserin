0bcd465a59ab622ac1aa63a67288c8d237ae38f2
BELHADJ KACEM <sofiane_belhadj_kacem@outlook.fr>
2022-02-27 15:30:01 +0100

    image base url

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 module.exports = {
+  images: {
+    deviceSizes: [320, 420, 768, 1024, 1200],
+    loader: "default",
+    domains: ["res.cloudinary.com"],
+  },
   i18n: {
     locales: ['en', 'fr'],
     defaultLocale: 'en',
   },
 }
+
